d3784612077803ba9e4aaed5591cfdb5bb387e0b
Nicholas Paro <nichparo@amazon.com>
2019-07-10 11:32:05 -0700

    features completed

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 **Author**: Nicholas Paro and Promila Tamang
 
-**Version**: 1.1.0\ (increment the patch/fix version number if you make more commits past your first submission)
+**Version**: 1.1.0
 
 ## Overview
-This application allows a user to search a specific location and retrieve a map, weather, events, movies, and yelp reviews from that location.
+This application allows a user to search a specific location and retrieve a map, weather, events, movies, and yelp reviews from that location based on the locations latitude and longitude.
 
 ## Getting Started
 Git clone and npm install for the application dependencies.
 
 ## Architecture
-Tehcnologies: JavaScript, Node.js, Express, dotenv, CORS, GitHub, Heroku
-APIs: Darksky, Google Maps geocode, Eventbrite
+Tehcnologies: JavaScript, Node.js, Express, dotenv, CORS, GitHub, Heroku, Postman
+APIs: Google Maps Geocode, Dark Sky, and Eventbrite
 
 ## Change Log
-### Lab-06
+### Lab-06 - 09 July 2019
 #### Feature 1
 * Number and name of feature: Location
 * Estimate of time needed to complete: 30 min
@@ -37,34 +37,34 @@ APIs: Darksky, Google Maps geocode, Eventbrite
 * Finish time: 11:45am
 * Actual time needed to complete: 15 min
 
-### Lab-07
+### Lab-07 - 10 July 2019
 #### Feature 1
 * Number and name of feature: Data Formatting
-* Estimate of time needed to complete: ____
-* Start time: ____
-* Finish time: ____
-* Actual time needed to complete: ____
+* Estimate of time needed to complete: 2 hours
+* Start time: 9:30am
+* Finish time: 11:30am
+* Actual time needed to complete: 2 hours
 
 #### Feature 2
 * Number and name of feature: Locations
-* Estimate of time needed to complete: ____
-* Start time: ____
-* Finish time: ____
-* Actual time needed to complete: ____
+* Estimate of time needed to complete: 30 min
+* Start time: 9:30am
+* Finish time: 10:00am
+* Actual time needed to complete: 30 min
 
 #### Feature 3
 * Number and name of feature: Weather
-* Estimate of time needed to complete: ____
-* Start time: ____
-* Finish time: ____
-* Actual time needed to complete: ____
+* Estimate of time needed to complete: 45 min
+* Start time: 10:00am
+* Finish time: 10:45am
+* Actual time needed to complete: 45 min
 
 #### Feature 4
 * Number and name of feature: Eventbrite
-* Estimate of time needed to complete: ____
-* Start time: ____
-* Finish time: ____
-* Actual time needed to complete: ____
+* Estimate of time needed to complete: 45 min
+* Start time: 10:45am
+* Finish time: 11:30am
+* Actual time needed to complete: 45 min
 
 ## Credits and Collaborations
 * Day 1: Nicholas Paro & Nhu Trinh
--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 // APP dependencies
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
@@ -17,9 +16,10 @@ app.use(cors());
 // app.get('/location', <function-reference>) is a route
 app.get('/location', searchToLatLng);
 app.get('/weather', searchToWeather);
-
+app.get('/events', searchToEvents);
 app.use('*', (request, response) => response.send('you got to the wrong place'));
 
+// Response builders
 function searchToLatLng(request, response){
   const locationName = request.query.data;
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationName}&key=${process.env.GEOCODE_API_KEY}`;
@@ -28,13 +28,13 @@ function searchToLatLng(request, response){
       const location = new Location(locationName, result);
       response.send(location);
     }).catch(e => {
-      console.error(e);
-      response.status(500).send(`Status 500: Sorry, something went wrong when getting ${locationName.formatted_query}`);
+      errors(response, e, locationName);
     });
 }
 
 function searchToWeather(request, response){
   const weatherData = request.query.data;
+  // console.log(weatherData);
   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${weatherData.latitude},${weatherData.latitude}`;
   const weatherArr = [];
   superagent.get(url)
@@ -42,22 +42,50 @@ function searchToWeather(request, response){
       result.body.daily.data.map(dailyWeather => weatherArr.push(new Weather(dailyWeather)));
       response.send(weatherArr);
     }).catch(e => {
-      console.error(e);
-      response.status(500).send(`Status 500: Sorry, something went wrong when getting the weather data for ${weatherData.formatted_query}`)
+      errors(response, e, weatherData);
     });
 }
 
+function searchToEvents(request, response){
+  const eventsData = request.query.data;
+  const url = `https://www.eventbriteapi.com/v3/events/search/?location.latitude=${eventsData.latitude}&location.longitude=${eventsData.longitude}&token=${process.env.EVENTBRITE_API_KEY}`;
+  const eventArr = [];
+  superagent.get(url)
+    .then( result => {
+      result.body.events.map(event => eventArr.push(new Event(event)));
+      response.send(eventArr);
+    })
+    .catch(e => {
+      errors(response, e, eventsData);
+    });
+}
+
+// Error message
+const errors = (response, e, location) => {
+  console.error(e);
+  response.status(500).send(`Status 500: Sorry, something went wrong when getting the data for ${location.formatted_query}`);
+}
+
+// Constructors
 function Location(query, result){
   this.search_query = query;
-  this.formatted_query = result.body.results[0].formatted_address,
-  this.latitude = result.body.results[0].geometry.location.lat,
-  this.longitude = result.body.results[0].geometry.location.lng
+  this.formatted_query = result.body.results[0].formatted_address;
+  this.latitude = result.body.results[0].geometry.location.lat;
+  this.longitude = result.body.results[0].geometry.location.lng;
 }
 
 function Weather(weatherData){
   const time = new Date(weatherData.time * 1000).toDateString();
   this.forecast = weatherData.summary;
   this.time = time;
+}
+
+function Event(event){
+  const date = new Date(event.start.local).toDateString();
+  this.link = event.url;
+  this.name = event.name.text;
+  this.event_date = date;
+  this.summary = event.description.text;
 }
 
 // Start the server
